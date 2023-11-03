34ed3e77d597142a87603549f503baae79968f5d
pablodawson <pablodawson2@gmail.com>
2023-11-03 13:19:46 -0300

    new

--- gaussian_to_unity/__init__.py
+++ gaussian_to_unity/__init__.py
@@ -15,21 +15,10 @@ from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianR
 from scene.gaussian_model import GaussianModel
 from utils.sh_utils import eval_sh
 from gaussian_to_unity.utils import *
+from gaussian_to_unity.converter import gaussian_timestep_to_unity
 import time as tm
 
 
-def get_order(pc : GaussianModel):
-
-    means3D = pc.get_xyz
-
-    # Calculate bounds
-    bounds_min, bounds_max = calculate_bounds(pc.get_xyz.cpu().numpy())
-    
-    # Morton sort
-    order = reorder_morton_job(bounds_min, 1.0/(bounds_max - bounds_min), means3D)
-    order_indexes = order[:,1].tolist()
-    
-    return order_indexes
 
 
 def save_frame(viewpoint_camera, pc : GaussianModel, pipe, scaling_modifier = 1.0, 
@@ -71,7 +60,6 @@ def save_frame(viewpoint_camera, pc : GaussianModel, pipe, scaling_modifier = 1.
     with torch.no_grad():
         pc._deformation_accum[deformation_point] += torch.abs(means3D_deform-means3D[deformation_point])
 
-    timestart = tm.time()
     means3D_final = torch.zeros_like(means3D)
     rotations_final = torch.zeros_like(rotations)
     scales_final = torch.zeros_like(scales)
@@ -81,47 +69,7 @@ def save_frame(viewpoint_camera, pc : GaussianModel, pipe, scaling_modifier = 1.
     means3D_final[~deformation_point] = means3D[~deformation_point]
     rotations_final[~deformation_point] = rotations[~deformation_point]
     scales_final[~deformation_point] = scales[~deformation_point]
-
-    print("array indexing time:", tm.time()-timestart)
-    # Keep the sorted order of the points
-
-    timestart = tm.time()
-    means3D_sorted = means3D_final[order_indexes].cpu().numpy().copy()
-
-    rotations_to_save, scales_linealized= linealize(rotations_final[order_indexes].cpu().numpy().copy(), 
-                                                  scales_final[order_indexes].cpu().numpy().copy())
     
-    print("linealization time:", tm.time()-timestart)
-
-    timestart = tm.time()
-    chunkSize = 256
-    means3D_to_save, scales_to_save, means_chunks, scale_chunks = create_chunks(means3D_sorted, scales_linealized, means3D.shape[0], chunkSize)
-    sh_index = None
-    
-    # Debug reconstruction
-    debug = False
-
-    if (debug):
-        pos_debug = means3D_to_save[0:chunkSize].copy()
-        min_pos_chunk = means_chunks[0][0]
-        max_pos_chunk = means_chunks[0][1]
-
-        pos_recon = min_pos_chunk + pos_debug * (max_pos_chunk - min_pos_chunk)
-
-        # Ensure correct reconstruction
-        print((pos_recon[0:20] - means3D_sorted[0:20] <= 1e-5).all())
-
-    print("chunk creation time:", tm.time()-timestart)
-    
-    timestart = tm.time()
-    create_positions_asset(means3D_to_save, basepath, format=pos_format, idx= idx)
-    print("create_positions_asset time:", tm.time()-timestart)
-    
-    #timestart = tm.time()
-    #create_others_asset(rotations_to_save, scales_to_save, sh_index, basepath, scale_format="Norm11", idx= idx)
-   # print("create_others_asset time:", tm.time()-timestart)
-
-    timestart = tm.time()
-    create_chunks_asset(means_chunks, scale_chunks, basepath, idx= idx)
-    print("create_chunks_asset time:", tm.time()-timestart)
-    
+    # Create Unity compatible frames for each gaussian state (only position at the moment)
+    gaussian_timestep_to_unity(means3D_final, scales_final, rotations_final, order_indexes, debug=True, 
+                               pos_format=pos_format, basepath=basepath, idx=idx)
